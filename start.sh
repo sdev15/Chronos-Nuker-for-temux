3348785549f5a9b41497df76ad607606d28105fd
sohaib <70706835+sdev15@users.noreply.github.com>
2024-05-01 16:21:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
+apt update
+apt upgrade 
+mkdir /storage/emulated/0/TermuxStorage
+apt install python
+
+echo "installing python"
 
 echo "Installing dependencies..."
 pip install -r requirements.txt
