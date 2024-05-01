90a3d6dcdc160c8e425ee8f15f5d843eaa40b93a
sohaib <70706835+sdev15@users.noreply.github.com>
2024-05-01 16:07:20 +0100

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,7 @@
-@echo off
-echo Installing dependencies...
-python3 -m pip install -r requirements.txt
-echo Starting...
-python3 src/main.py
+#!/data/data/com.termux/files/usr/bin/bash
+
+echo "Installing dependencies..."
+pip install -r requirements.txt
+
+echo "Starting..."
+python src/main.py
